e04783ce89a8b158c38e7d81c907bb18e67a12c0
bimosyah <bimosyahputro88@gmail.com>
2020-06-02 12:58:15 +0700

    customer search

--- app/src/main/java/bimo/syahputro/chromeinc/activity/tambahTransaksi/lama/DaftarCustomerActivity.java
+++ app/src/main/java/bimo/syahputro/chromeinc/activity/tambahTransaksi/lama/DaftarCustomerActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -114,6 +115,24 @@ public class DaftarCustomerActivity extends AppCompatActivity {
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_customer, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.menu_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                daftarCustomerAdapter.getFilter().filter(s);
+                return false;
+            }
+        });
+
         return true;
     }
 }
--- app/src/main/java/bimo/syahputro/chromeinc/activity/tambahTransaksi/lama/DaftarCustomerAdapter.java
+++ app/src/main/java/bimo/syahputro/chromeinc/activity/tambahTransaksi/lama/DaftarCustomerAdapter.java
@@ -4,24 +4,60 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import bimo.syahputro.chromeinc.R;
 import bimo.syahputro.chromeinc.network.entity.Customer;
 
-class DaftarCustomerAdapter extends RecyclerView.Adapter<DaftarCustomerAdapter.ViewHolder> {
+class DaftarCustomerAdapter extends RecyclerView.Adapter<DaftarCustomerAdapter.ViewHolder> implements Filterable {
     Context context;
     List<Customer> customerList;
+    List<Customer> customerListFull;
+    private Filter daftarCustomerFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            List<Customer> filteredList = new ArrayList<>();
+
+            if (charSequence == null || charSequence.length() == 0) {
+                filteredList.addAll(customerListFull);
+            } else {
+                String filterPattern = charSequence.toString().toLowerCase().trim();
+
+                for (Customer customer : customerListFull) {
+                    if (customer.getNamaCustomer().toLowerCase().contains(filterPattern)) {
+                        filteredList.add(customer);
+                    }
+                }
+            }
+
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            customerList.clear();
+            customerList.addAll((List) filterResults.values);
+            notifyDataSetChanged();
+        }
+
+    };
 
     public DaftarCustomerAdapter(Context context, List<Customer> customerList) {
         this.context = context;
         this.customerList = customerList;
+        this.customerListFull = new ArrayList<>(customerList);
     }
 
     @NonNull
@@ -41,7 +77,12 @@ class DaftarCustomerAdapter extends RecyclerView.Adapter<DaftarCustomerAdapter.V
         return customerList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    @Override
+    public Filter getFilter() {
+        return daftarCustomerFilter;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
         TextView tvNamaCustomer;
         ConstraintLayout container;
 
