7f6ca4f6b8f2882c7a19f7300aca8470d87a2c3d
bimosyah <bimosyahputro88@gmail.com>
2020-06-03 21:08:35 +0700

    get data from customer fragment and fix header color

--- app/src/main/java/bimo/syahputro/chromeinc/activity/tambahTransaksi/baru/TransaksiCustomerBaruActivity.java
+++ app/src/main/java/bimo/syahputro/chromeinc/activity/tambahTransaksi/baru/TransaksiCustomerBaruActivity.java
@@ -1,31 +1,38 @@
 package bimo.syahputro.chromeinc.activity.tambahTransaksi.baru;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import bimo.syahputro.chromeinc.R;
 import bimo.syahputro.chromeinc.activity.tambahTransaksi.baru.fragment.BarangFragment;
 import bimo.syahputro.chromeinc.activity.tambahTransaksi.baru.fragment.CheckoutFragment;
 import bimo.syahputro.chromeinc.activity.tambahTransaksi.baru.fragment.CustomerFragment;
 
-public class TransaksiCustomerBaruActivity extends AppCompatActivity {
+public class TransaksiCustomerBaruActivity extends AppCompatActivity implements CustomerFragment.OnDataPassCustomer {
     public static String ID_CUSTOMER = "ID_CUSTOMER";
     public static String NAMA_CUSTOMER = "NAMA_CUSTOMER";
     public static String ALAMAT_CUSTOMER = "ALAMAT_CUSTOMER";
     public static String NOTELP_CUSTOMER = "NOTELP_CUSTOMER";
     Fragment fragmentCustomer, fragmentBarang, fragmentCheckout;
     ImageView ivKiri, ivTengah, ivKanan;
+    TextView tvKiri, tvTengah, tvKanan;
     int posisi_fragment = 1;
     LinearLayout btnBack, btnNext;
+    String namaCustomer, alamatCustomer, notelpCustomer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,13 +49,23 @@ public class TransaksiCustomerBaruActivity extends AppCompatActivity {
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (posisi_fragment == 1){
-                    openFragment(fragmentBarang);
+                if (posisi_fragment == 1) {
                     posisi_fragment = 2;
+
+                    openFragment(fragmentBarang);
+                    callCustomerData();
+
+                    tvKiri.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.transaksi_header));
+                    tvTengah.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
                     ivKiri.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_brightness, getApplicationContext().getTheme()));
-                }else if (posisi_fragment == 2){
-                    openFragment(fragmentCheckout);
+
+                } else if (posisi_fragment == 2) {
                     posisi_fragment = 3;
+
+                    openFragment(fragmentCheckout);
+
+                    tvTengah.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.transaksi_header));
+                    tvKanan.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
                     ivTengah.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_brightness, getApplicationContext().getTheme()));
                 }
             }
@@ -57,20 +74,28 @@ public class TransaksiCustomerBaruActivity extends AppCompatActivity {
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (posisi_fragment == 3){
-                    openFragment(fragmentBarang);
+                if (posisi_fragment == 3) {
                     posisi_fragment = 2;
+
+                    openFragment(fragmentBarang);
+
+                    tvKanan.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.transaksi_header));
+                    tvTengah.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
                     ivTengah.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_adjust, getApplicationContext().getTheme()));
-                }else if (posisi_fragment == 2){
-                    openFragment(fragmentCustomer);
+                } else if (posisi_fragment == 2) {
                     posisi_fragment = 1;
+
+                    openFragment(fragmentCustomer);
+
+                    tvKiri.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
+                    tvTengah.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.transaksi_header));
                     ivKiri.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_adjust, getApplicationContext().getTheme()));
                 }
             }
         });
     }
 
-    private void openFragment(final Fragment fragment)   {
+    private void openFragment(final Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.container_transaksi, fragment);
@@ -78,10 +103,22 @@ public class TransaksiCustomerBaruActivity extends AppCompatActivity {
         transaction.commit();
     }
 
+    private void callCustomerData() {
+        CustomerFragment fragment = (CustomerFragment) getSupportFragmentManager().findFragmentById(R.id.container_transaksi);
+        if (fragment != null) {
+            fragment.passData();
+        }
+        Snackbar.make(btnNext, namaCustomer + " " + notelpCustomer + " " + alamatCustomer, Snackbar.LENGTH_SHORT)
+                .show();
+    }
 
-    private void init(){
+
+    private void init() {
         ivKiri = findViewById(R.id.iv_posisi_kiri);
         ivTengah = findViewById(R.id.iv_posisi_tengah);
+        tvKanan = findViewById(R.id.tv_posisi_kanan);
+        tvKiri = findViewById(R.id.tv_posisi_kiri);
+        tvTengah = findViewById(R.id.tv_posisi_tengah);
         ivKanan = findViewById(R.id.iv_posisi_kanan);
         fragmentCustomer = new CustomerFragment();
         fragmentBarang = new BarangFragment();
@@ -102,5 +139,12 @@ public class TransaksiCustomerBaruActivity extends AppCompatActivity {
 
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
+    }
+
+    @Override
+    public void passDataCustomer(String nama, String alamat, String notelp) {
+        namaCustomer = nama;
+        alamatCustomer = alamat;
+        notelpCustomer = notelp;
     }
 }
--- app/src/main/java/bimo/syahputro/chromeinc/activity/tambahTransaksi/baru/fragment/CustomerFragment.java
+++ app/src/main/java/bimo/syahputro/chromeinc/activity/tambahTransaksi/baru/fragment/CustomerFragment.java
@@ -1,9 +1,11 @@
 package bimo.syahputro.chromeinc.activity.tambahTransaksi.baru.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +14,9 @@ import androidx.fragment.app.Fragment;
 import bimo.syahputro.chromeinc.R;
 
 public class CustomerFragment extends Fragment {
+    OnDataPassCustomer dataParser;
+    EditText etNamaCustomer, etAlamat, etNoTelp;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -21,5 +26,25 @@ public class CustomerFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        etNamaCustomer = view.findViewById(R.id.et_nama_customer);
+        etAlamat = view.findViewById(R.id.et_alamat_customer);
+        etNoTelp = view.findViewById(R.id.et_notelp_customer);
+    }
+
+    public void passData(){
+        dataParser.passDataCustomer(etNamaCustomer.getText().toString(),etAlamat.getText().toString(), etNoTelp.getText().toString());
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        dataParser = (OnDataPassCustomer) context;
+
+    }
+
+    public interface OnDataPassCustomer {
+        void passDataCustomer(String nama, String alamat, String notelp);
     }
 }
+
+
